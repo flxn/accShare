3d12c5cc029f92a95381367e25ec5efe84985c26
Felix Stein <felix.stein.vb@gmail.com>
2014-11-28 19:34:53 +0000

    materialize 2

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,9 +1,16 @@
-window.scrollTo(0, 0);
-document.querySelector('.addnewcard').style.display = "inherit";
+$(document).ready(function(){
+  $('ul.tabs').tabs();
+  $(".button-collapse").sideNav();
+  window.scrollTo(0, 0);
+  var matches = document.querySelectorAll('.hide-load');
 
-var matches = document.querySelectorAll('.hide-load');
+  for (var i in matches) {
+    if(matches[i]){
+      matches[i].style.transition = "opacity 0.3s";
+      matches[i].style.opacity = "1";
+    }
+      
+  }
+});
 
-for (var i in matches) {
-    matches[i].style.transition = "opacity 0.3s";
-    matches[i].style.opacity = "1";
-}
+
